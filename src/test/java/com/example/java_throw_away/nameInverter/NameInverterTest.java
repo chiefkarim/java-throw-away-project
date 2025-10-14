20e9876553295fe63b7631a62533eaf4012915cd
chiefkarim <krimotop2016@gmail.com>
2025-10-14 17:24:48 +0100

    feat: honorofics and post nominals

--- src/test/java/com/example/java_throw_away/nameInverter/NameInverterTest.java
+++ src/test/java/com/example/java_throw_away/nameInverter/NameInverterTest.java
@@ -2,6 +2,10 @@ package com.example.java_throw_away.nameInverter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 public class NameInverterTest {
@@ -39,17 +43,52 @@ public class NameInverterTest {
     assertInvertedNameEquals(" First   Last", "Last, First");
   }
 
+  @Test
+  public void ignoreHonorific() throws Exception {
+    assertInvertedNameEquals("Mr. First Last", "Last, First");
+    assertInvertedNameEquals("Mrs. First Last", "Last, First");
+  }
+
+  @Test
+  public void postNominals_stayAtEnd() throws Exception {
+    assertInvertedNameEquals("First Last Sr.", "Last, First Sr.");
+    assertInvertedNameEquals("First Last BS. Phd.", "Last, First BS. Phd.");
+  }
+
   private String invertName(String name) {
     if (isNameNullOrEmpty(name))
       return "";
     else {
-      String names[] = name.trim().split("\\s+");
-      if (names.length == 1)
-        return names[0];
+      List<String> names = splitNames(name);
+      if (names.size() > 1 && isHonorific(names.get(0)))
+        names.remove(0);
+      if (isPostNominal(names)) {
+        List<String> postNominals;
+        postNominals = names.subList(2, names.size());
+        String postNominal = "";
+        for (String pn : postNominals)
+          postNominal += pn + " ";
+        return String.format("%s, %s %s", names.get(1), names.get(0), postNominal).trim();
+      }
+      if (names.size() == 1)
+        return names.get(0);
       else {
-        return String.format("%s, %s", names[1], names[0]);
+        return String.format("%s, %s", names.get(1), names.get(0));
       }
     }
+  }
+
+  private boolean isPostNominal(List<String> names) {
+    return names.size() > 2;
+  }
+
+  private boolean isHonorific(String word) {
+    return word.matches("Mr\\.|Mrs\\.");
+  }
+
+  private List<String> splitNames(String name) {
+    List<String> names = new ArrayList<String>(Arrays.asList(name.trim().split("\\s+")));
+    return names;
   }
 
   private boolean isNameNullOrEmpty(String name) {
