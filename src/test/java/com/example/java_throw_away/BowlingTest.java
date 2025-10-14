71364ea843507dfaa1b30bc46bf70a11132068c2
chiefkarim <krimotop2016@gmail.com>
2025-10-14 14:32:29 +0100

    feat: invert name first implimentation with TDD

--- src/test/java/com/example/java_throw_away/BowlingTest.java
+++ src/test/java/com/example/java_throw_away/BowlingTest.java
@@ -2,7 +2,6 @@ package com.example.java_throw_away;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingTest {
