925831ab1d8d27d7267c3ba561b5520339271ff6
chiefkarim <krimotop2016@gmail.com>
2025-11-10 15:13:49 +0100

    feat: add schema types to args parser

--- src/test/java/com/example/java_throw_away/argsParser/ArgsParserTest.java
+++ src/test/java/com/example/java_throw_away/argsParser/ArgsParserTest.java
@@ -1,6 +1,8 @@
 package com.example.java_throw_away.argsParser;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Map;
 
@@ -9,12 +11,76 @@ import org.junit.jupiter.api.Test;
 public class ArgsParserTest {
 
   @Test
-  public void shouldThrowError_WhenPassedInSchemaWithoutTypes() throws Exception {
-    assertThrows(IllegalArgumentException.class, () -> parseArgs(Map.of(1, "karim")));
+  public void schemaFlag_ShouldHaveTypeAndNumberPropertiesWithCorrectTypes() throws Exception {
+    SchemaFlag exampleFlag = new SchemaFlag(String.class, 0);
+    assertTrue(exampleFlag.number instanceof Integer);
+    assertTrue(exampleFlag.type == String.class);
   }
-//TODO:: switch to an object for the schema for extendability and testability
-  private void parseArgs(Map<String,Map<String,Object>> Schema) {
 
+  @Test
+  public void schemaFlag_ShouldHaveZeroAsDefaultNumberValueAndStringAsDefaultType() throws Exception {
+    SchemaFlag exampleFlag = new SchemaFlag(null, null);
+    assertTrue(exampleFlag.number == 0);
+    assertTrue(exampleFlag.type == String.class);
+  }
+
+  @Test
+  public void schema_ShouldInitiateSchemaCorrectly() throws Exception {
+    Schema exampleSechema = new Schema(Map.of("-c", new SchemaFlag(null, null), "-a", new SchemaFlag(String.class, 1),
+        "-b", new SchemaFlag(Integer.class, 2)));
+    assertEquals(String.class, exampleSechema.schema.get("-c").type);
+    assertEquals(0, exampleSechema.schema.get("-c").number);
+    assertEquals(String.class, exampleSechema.schema.get("-a").type);
+    assertEquals(1, exampleSechema.schema.get("-a").number);
+    assertEquals(Integer.class, exampleSechema.schema.get("-b").type);
+    assertEquals(2, exampleSechema.schema.get("-b").number);
+  }
+
+  @Test
+  public void schema_shouldThrowErrorIfSchemaKeyDoesntStartWithMinus() throws Exception {
+    Map<String, SchemaFlag> InvalidSchema = Map.of("ch", new SchemaFlag(String.class, 2));
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      new Schema(InvalidSchema);
+    });
+    assertEquals("Flag name must start with minus sign.", exception.getMessage());
+  }
+
+  @Test
+  public void schema_shouldThrowErrorIfSchemaKeyIsNotOneChar() throws Exception {
+    Map<String, SchemaFlag> InvalidSchema = Map.of("-char", new SchemaFlag(String.class, 2));
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      new Schema(InvalidSchema);
+    });
+    assertEquals("Flag name must be one char long.", exception.getMessage());
+  }
+
+  class Schema {
+    private Map<String, SchemaFlag> schema;
+
+    Schema(Map<String, SchemaFlag> schema) {
+      for (String key : schema.keySet()) {
+        if (!key.startsWith("-")) {
+          throw new IllegalArgumentException("Flag name must start with minus sign.");
+        }
+        if (key == null || key.length() != 2) {
+          throw new IllegalArgumentException("Flag name must be one char long.");
+        }
+
+      }
+      this.schema = schema;
+    }
+  }
+
+  class SchemaFlag {
+    private Class<?> type = String.class;
+    private Integer number = 0;
+
+    SchemaFlag(Class<?> type, Integer number) {
+      if (type != null)
+        this.type = type;
+      if (number != null)
+        this.number = number;
+    }
 
   }
 
